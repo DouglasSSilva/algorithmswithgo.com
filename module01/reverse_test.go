951305bfa30fea915b23770bd0560f7e794b6f19
Douglas Sales <douglassales@residuall.org>
2022-01-31 16:46:15 -0300

    exercise-003: reverse a string

--- module01/reverse_test.go
+++ module01/reverse_test.go
@@ -12,7 +12,14 @@ func TestReverse(t *testing.T) {
 	}{
 		{"cat", "tac"},
 		{"alphabet", "tebahpla"},
-		// {"日本語", "語本日"},
+		{"日本語", "語本日"},
+		{"X", "X"},
+		{"b\u0301", "b\u0301"},
+		{"😎⚽", "⚽😎"},
+		{"Les Mise\u0301rables", "selbare\u0301siM seL"},
+		{"ab\u0301cde", "edcb\u0301a"},
+		{"This `\xc5` is an invalid UTF8 character", "retcarahc 8FTU dilavni na si `�` sihT"},
+		{"The quick bròwn 狐 jumped over the lazy 犬", "犬 yzal eht revo depmuj 狐 nwòrb kciuq ehT"},
 	}
 	for _, tc := range tests {
 		t.Run(fmt.Sprintf("%v", tc.word), func(t *testing.T) {
