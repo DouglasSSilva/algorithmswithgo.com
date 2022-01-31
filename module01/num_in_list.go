276e2e7cbbffa2683f5c884ea39389aedb420d88
Douglas Sales <douglassales@residuall.org>
2022-01-31 16:33:50 -0300

    feature: sum list and find number in list

--- module01/num_in_list.go
+++ module01/num_in_list.go
@@ -3,5 +3,11 @@ package module01
 // NumInList will return true if num is in the
 // list slice, and false otherwise.
 func NumInList(list []int, num int) bool {
+
+	for i := range list {
+		if num == list[i] {
+			return true
+		}
+	}
 	return false
 }
