b19e243db76c5bec38addec3553f38a2a3a616f7
DouglasSSilva <wdouglassales@gmail.com>
2022-02-03 09:43:38 -0300

    feature: bse to base code

--- module01/base_to_base.go
+++ module01/base_to_base.go
@@ -1,5 +1,7 @@
 package module01
 
+import "fmt"
+
 // BaseToBase takes in a number, the base it is currently
 // in, and the base you want it to be converted to. It then
 // returns a string representing the number in the new base.
@@ -9,5 +11,12 @@ package module01
 //   BaseToBase("E", 16, 2) => "1110"
 //
 func BaseToBase(value string, base, newBase int) string {
-	return ""
+	val := BaseToDec(value, base)
+	if newBase == 10 {
+		valString := fmt.Sprintf("%d", val)
+		return valString
+
+	}
+
+	return DecToBase(val, newBase)
 }
