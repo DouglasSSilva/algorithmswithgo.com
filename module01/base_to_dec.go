a07b691371b91c3b53d37e5ca66f0b724485eb19
Douglas Sales <wdouglassales@gmail.com>
2022-02-02 22:16:53 -0300

    exercise: base to decimal

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,5 +1,9 @@
 package module01
 
+import (
+	"math"
+)
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
@@ -9,5 +13,29 @@ package module01
 //   BaseToDec("1110", 2) => 14
 //
 func BaseToDec(value string, base int) int {
-	return 0
+
+	valueSize := len(value)
+
+	total := float64(0)
+	for i := 0; i < valueSize; i++ {
+		numberAsString := string(value[valueSize-(i+1)])
+		position := findPosition(numberAsString)
+
+		total += position * math.Pow(float64(base), float64(i))
+
+	}
+	return int(total)
+}
+
+func findPosition(numberAsString string) float64 {
+	modString := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"}
+
+	position := 0
+	for i, _ := range modString {
+		if modString[i] == numberAsString {
+			position = i
+		}
+	}
+
+	return float64(position)
 }
